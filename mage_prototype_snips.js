4f9381b947c90571076de097898268bcaa36cf7b
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-04-26 18:11:54 +0530

    Update mage_prototype_snips.js

--- mage_prototype_snips.js
+++ mage_prototype_snips.js
@@ -1,5 +1,5 @@
 //Validate by confirmation of fields
-$('confirm_email').addClassName('validate-custemail');
+/*$('confirm_email').addClassName('validate-custemail');
 Validation.addAllThese([
 	    ['validate-custemail', 'Please make sure your emails match.', function(v) {
 	        var conf = $('confirmation') ? $('confirmation') : $$('.validate-custemail')[0];
@@ -9,6 +9,33 @@ Validation.addAllThese([
 	        	pass = $('email');
 	        }
 	        confirm =conf.value;
+	        if(!confirm && $('confirm_email')) {
+	        	confirm = $('confirm_email').value;
+	        }
+	        return (pass.value == confirm);
+	    }],
+	]);*/
+	
+		$('confirm_email').addClassName('validate-cemail');
+        Validation.addAllThese([
+	    ['validate-cemail', 'Please make sure your emails match.', function(v) {
+	        //var conf = $('confirm_email') ? $('confirm_email') : $$('.validate-cemail')[0];
+	        var conf = $$('.validate-cemail')[0];
+	        var pass = false;
+	        var confirm;
+	        var emailElements = $$('.validate-email');
+	        
+	        for (var i = 0; i < emailElements.size(); i++) {
+	        	var emailElement = emailElements[i];
+	        	if (emailElement.up('form').id == conf.up('form').id) {
+	        		pass = emailElement;
+	        	}
+	        }
+
+	        if ($('email')) {
+	        	pass = $('email');
+	        }
+	        confirm = conf.value;
 	        if(!confirm && $('confirm_email')) {
 	        	confirm = $('confirm_email').value;
 	        }
